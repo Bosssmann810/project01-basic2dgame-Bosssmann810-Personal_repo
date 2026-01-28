853422623ee7a37b8f44d96273cfb8d64c13c5e3
Bosssmann810 <sealered@gmail.com>
2026-01-28 14:50:05 -0400

    Initial commit

---
+++ Assets/playerinputs.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerinputs : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    public Vector2 inputMovement;
+    public float speed;
+    public GameObject normal;
+    public GameObject upsideDown;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        upsideDown.SetActive(false);
+        normal.SetActive(true);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputMovement = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + inputMovement * speed * Time.fixedDeltaTime);
+    }
+    public void OnFlip(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            normal.SetActive(false);
+            upsideDown.SetActive(true);
+
+        }
+        if (context.canceled)
+        {
+            normal.SetActive(true);
+            upsideDown.SetActive(false);
+        }
+    }
+}
