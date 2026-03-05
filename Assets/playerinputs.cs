781cc5aaa6701b5457a6b2e582e4a71f991f5b47
Bosssmann810 <sealered@gmail.com>
2026-03-05 12:03:29 -0400

    animations and sprites added

--- Assets/playerinputs.cs
+++ Assets/playerinputs.cs
@@ -7,16 +7,26 @@ public class playerinputs : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody2D rb;
     public Vector2 inputMovement;
+    public Vector2 oldMovement;
     public float speed;
     public GameObject normal;
     public GameObject upsideDown;
     public Servicehub servicehub;
+
+    [SerializeField] private Animator playerAnimController;
+
+    private int MoveInputXHash = Animator.StringToHash("MoveInputX");
+    private int MoveInputYHash = Animator.StringToHash("MoveInputY");
+    private int IsMovingHash = Animator.StringToHash("IsMoving");
+    private int OldXHash = Animator.StringToHash("OldInputX");
+    private int OldYHash = Animator.StringToHash("OldInputY");
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         upsideDown.SetActive(false);
         normal.SetActive(true);
+        playerAnimController = GetComponentInChildren<Animator>();
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -26,6 +36,11 @@ public class playerinputs : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + inputMovement * speed * Time.fixedDeltaTime);
+
+    }
+    public void Update()
+    {
+        HandleAnimation();
     }
     public void OnFlip(InputAction.CallbackContext context)
     {
@@ -47,5 +62,21 @@ public class playerinputs : MonoBehaviour
         servicehub.gameStateManager.TogglePause();
 
     }
+    public void HandleAnimation()
+    {
+        if (inputMovement != Vector2.zero )
+        {
+            playerAnimController.SetBool(IsMovingHash, true);
+            playerAnimController.SetFloat(MoveInputXHash, inputMovement.x);
+            playerAnimController.SetFloat(MoveInputYHash, inputMovement.y);
+            oldMovement = inputMovement;
+        }
+        else if (inputMovement == Vector2.zero )
+        {
+            playerAnimController.SetBool(IsMovingHash, false);
+            playerAnimController.SetFloat(OldYHash, oldMovement.y);
+            playerAnimController.SetFloat(OldXHash, oldMovement.x);
+        }
+    } 
 }
 
